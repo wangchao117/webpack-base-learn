ea017d16d7405dc74cb9a8feb80fd88fc6fa4fa2
wangchao <wcd126yx@126.com>
2018-11-24 20:55:32 +0800

    css模块

--- dist/main.js
+++ dist/main.js
@@ -1,86 +1,48 @@
-(function (modules) {
-  // 模块的缓存
-  var installedModules = {};
-  // 申明一个require方法
-  function __webpack_require__(moduleId) {
-    // 如果缓存已经有了人，说明模块已经加载过了
-    if (installedModules[moduleId]) {
-      return installedModules[moduleId].exports;
+! function (e) {
+    var t = {};
+
+    function n(r) {
+        if (t[r]) return t[r].exports;
+        var o = t[r] = {
+            i: r,
+            l: !1,
+            exports: {}
+        };
+        return e[r].call(o.exports, o, o.exports, n), o.l = !0, o.exports
     }
-    // 没有加载过就创建一个模块，并放入缓存
-    var module = installedModules[moduleId] = {
-      i: moduleId,
-      l: false,
-      exports: {}
-    };
-    // 执行模块函数 modules[moduleId]其实就是下面对象的函数体
-    modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-    // 标记模块是加载过的
-    module.l = true;
-    // 最后返回导出对象 
-    return module.exports;
-    
-  }
-  // 向外暴露模块对象
-  __webpack_require__.m = modules;
-  // 向外暴露模块缓存
-  __webpack_require__.c = installedModules;
-  // 定义getter方法，为了兼容
-  __webpack_require__.d = function (exports, name, getter) {
-    if (!__webpack_require__.o(exports, name)) {
-      // 在expport对象定义name属性，它的值是不可配置的，可枚举的，并指定获取器
-      Object.defineProperty(exports, name, {
-        enumerable: true,
-        get: getter
-      });
-    }
-  }; 
-  // 在到处对象定义__esModule对象，为了兼容
-  __webpack_require__.r = function (exports) {
-    if (typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-      Object.defineProperty(exports, Symbol.toStringTag, {
-        value: 'Module'
-      });
-    }
-    Object.defineProperty(exports, '__esModule', {
-      value: true
-    });
-  };
-  __webpack_require__.t = function (value, mode) {
-    if (mode & 1) value = __webpack_require__(value);
-    if (mode & 8) return value;
-    if ((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
-    var ns = Object.create(null);
-    __webpack_require__.r(ns);
-    Object.defineProperty(ns, 'default', {
-      enumerable: true,
-      value: value
-    });
-    if (mode & 2 && typeof value != 'string')
-      for (var key in value) __webpack_require__.d(ns, key, function (key) {
-        return value[key];
-      }.bind(null, key));
-    return ns;
-  };
-  __webpack_require__.n = function (module) {
-    var getter = module && module.__esModule ?
-      function getDefault() {
-        return module['default'];
-      } :
-      function getModuleExports() {
-        return module;
-      };
-    __webpack_require__.d(getter, 'a', getter);
-    return getter;
-  };
-  __webpack_require__.o = function (object, property) {
-    return Object.prototype.hasOwnProperty.call(object, property);
-  };
-  __webpack_require__.p = "";
-  return __webpack_require__(__webpack_require__.s = "./src/index.js");
-})
-({
-  "./src/index.js": (function (module, exports) {
-    eval("console.log('hello')\ndocument.getElementById('app').innerHTML='webpack'\n\n//# sourceURL=webpack:///./src/index.js?");
-  })
-});
+    n.m = e, n.c = t, n.d = function (e, t, r) {
+        n.o(e, t) || Object.defineProperty(e, t, {
+            enumerable: !0,
+            get: r
+        })
+    }, n.r = function (e) {
+        "undefined" != typeof Symbol && Symbol.toStringTag && Object.defineProperty(e, Symbol.toStringTag, {
+            value: "Module"
+        }), Object.defineProperty(e, "__esModule", {
+            value: !0
+        })
+    }, n.t = function (e, t) {
+        if (1 & t && (e = n(e)), 8 & t) return e;
+        if (4 & t && "object" == typeof e && e && e.__esModule) return e;
+        var r = Object.create(null);
+        if (n.r(r), Object.defineProperty(r, "default", {
+                enumerable: !0,
+                value: e
+            }), 2 & t && "string" != typeof e)
+            for (var o in e) n.d(r, o, function (t) {
+                return e[t]
+            }.bind(null, o));
+        return r
+    }, n.n = function (e) {
+        var t = e && e.__esModule ? function () {
+            return e.default
+        } : function () {
+            return e
+        };
+        return n.d(t, "a", t), t
+    }, n.o = function (e, t) {
+        return Object.prototype.hasOwnProperty.call(e, t)
+    }, n.p = "", n(n.s = 0)
+}([function (e, t) {
+    console.log("hello"), requie("./index.css"), document.getElementById("app").innerHTML = "webpack"
+}]);
--- webpack-config.js
+++ webpack-config.js
@@ -8,7 +8,16 @@ module.exports = {
         filename: 'bundle.js' // 打包后的文件名
     },
     module: { // 模块--一切解释模块
-
+        // 规则
+        rules: [
+            {
+                test: /\.css$/, // 转换文件的配置规则，以css结尾就走这个
+                // css-loader用来解析处理css文件中的URL处理路径，要把一个css文件变成一个模块
+                // style-loader可把css文件变成style标签插入header中
+                // 多个loader是有顺序的，从右往左写，因为转化的时候是葱油往左转换
+                loader: ['style-loader', 'css-loader'] // 两个一个不能少
+            },
+        ]
     },
     piugins:  [ // 插件
 
