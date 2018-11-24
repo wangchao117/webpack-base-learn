5d78373bd087ddccf92b6e60eea0b2e54af5fae9
wangchao <wcd126yx@126.com>
2018-11-24 22:25:16 +0800

    插件

--- webpack-config.js
+++ webpack-config.js
@@ -1,11 +1,14 @@
 const path = require('path'); 
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 // webpack内部有一个事件流
 
 module.exports = {
     entry: './src/index.js', // 入口
     output: { // 输出
         path: path.join(__dirname, 'dist'), // 输出的文件夹，只能是绝对路径 
-        filename: 'bundle.js' // 打包后的文件名
+        // filename: 'bundle.js' // 打包后的文件名
+        // name是entry名字，默认是main，hash根据打包后的文件内容计算出来的一个hash值，：8是取8位
+        filename: '[name].[hash:8].js'
     },
     module: { // 模块--一切解释模块
         // 规则
@@ -20,7 +23,12 @@ module.exports = {
         ]
     },
     piugins:  [ // 插件
-
+        // 此插件能自动产出html文件
+        new HtmlWebpackPlugin({
+            template: './src/index.html', // 直接产出的模版
+            filename: 'index.html',
+            title: '欢迎使用插件'
+        })
     ],
     devSever: { // 配置京台文件服务器，可以用来预览打包后的项目
         contentBase: './dist',
